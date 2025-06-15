d7442cf7de8dfc1c1a9e90186bd5da0b37405fe3
renaxalv <renansanalves@gmail.com>
2025-06-15 10:49:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,35 @@
-const CACHE_NAME = 'pwa-v2';
-const ASSETS = [
-  '/PWA-teste/',
-  '/PWA-teste/index.html',
-  '/PWA-teste/manifest.json',
-  '/PWA-teste/assets/icon-192.png',
-  '/PWA-teste/sw.js'
+const CACHE_NAME = 'pwa-complete-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://raw.githubusercontent.com/renaxalv/pwa-assets-luft/main/icon-192.png',
+  'https://raw.githubusercontent.com/renaxalv/pwa-assets-luft/main/icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
